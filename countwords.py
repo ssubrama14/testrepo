35575bafed1ece85147c976b76aaa4f95e1ad90e
ssubrama14 <ssubrama14@gmail.com>
2021-03-27 20:18:57 -0700

    Add files via upload

---
+++ countwords.py
@@ -0,0 +1,15 @@
+input = "This is a test.  This is another test"
+mydict = {}
+with open("/Users/ssubrama/Documents/file.txt",'r') as f:
+   textList = f.readlines()
+
+for line in textList:
+    lineList = line.split(" ")
+    for word in lineList:
+       if word in mydict.keys():
+           mydict[word] = mydict[word] + 1
+       else:
+           mydict[word] = 1
+
+for k,v in mydict.items():
+    print("{0} occurs {1} times in the the input file".format(k,v))
